bc79ee6f0d057385aabb98111e9e8da33efe3147
ndtu <77379218+ndtu@users.noreply.github.com>
2021-02-09 17:47:35 -0800

    Update InfectionTracking.java

--- InfectionTracking.java
+++ InfectionTracking.java
@@ -1,14 +1,21 @@
 import java.io.File;
-import java.util.Scanner;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class InfectionTracking {
 
-public static int[] populateArrays(String pathToFile, String[] names,
-                           int[] locations, int[] movements,
-                           int[] infections)throws IOException; 
-   
-File FirstFile = new File(pathToFile);
-Scanner input = new Scanner(FirstFile);
-
-
+public static int populateArrays(String pathToFile, String[] names,
+int[] locations, int[] movements,
+int[] infections) throws IOException{
+    File FirstFile = new File(pathToFile);
+    Scanner input = new Scanner(FirstFile);
+    String[] First = new String[names.length];
+    int[] second = new int[locations.length];
+    int[] third = new int[movements.length];
+    int[] fourth = new int[infections.length];
+    
+    
+    return 9;
+    
+}
